ff4c07c258e74af47f83cecb89efaf5d7fc9814d
jesseziros <jtziros2020@gmail.com>
2020-12-19 13:45:57 -0500

    Completed user Schema

---
+++ models/users.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      require: "Name is Required"
+    },
+    username: {
+      type: String,
+      trim: true,
+      require: "Username is Required."
+    },
+    password: {
+      type: String,
+      trim: true,
+      require: "Password is Required."
+    },
+    age: {
+      type: Number
+    },
+    email: {
+      type: String,
+      trim: true
+    },
+    phone: {
+      type: String
+    },
+    photo: {
+      type: String
+    }
+  }
+)
