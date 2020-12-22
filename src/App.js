8b8c23e4e239b76f844638921c31d366098e31f6
8bitk9 <67807672+dallisconner@users.noreply.github.com>
2020-12-21 20:30:17 -0500

    Styling changes for the landing page

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import Navbar from "./components/Navbar";
 import NavTabs from "./components/NavTabs"
 import Footer from "./components/Footer";
 import Wrapper from "./components/Wrapper";
-import logo from './logo.png';
 import './App.css';
 import NonUserProfiles from "./pages/NonUserProfiles";
 import Profile from "./pages/Profile";
@@ -24,7 +23,6 @@ function App() {
       <NavTabs />
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <div>
             <Wrapper>
               <Route exact path="/" component={Landing} />
