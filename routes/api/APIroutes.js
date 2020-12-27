e2f9d8087648af41302fa37e513669d7d669c5ca
Jesse Ziros <jtziros2020@gmail.com>
2020-12-27 12:29:32 -0500

    File no longer exists

--- routes/api/APIroutes.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router();
-const userDataController = require("../../controllers/userDataController");
-
-// Matches with "/api/userData"
-router.route("/signup")
-    .post(userDataController.create);
-
-module.exports = router;
