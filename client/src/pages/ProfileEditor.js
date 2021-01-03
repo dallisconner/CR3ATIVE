25f5a32014afbdc734b5af79d151baf501e52688
Tristan Eastman <teastman85@gmail.com>
2021-01-03 06:14:19 -0500

    Add backend connection for Profile Editor page. Set redirect to Dashboard.

--- client/src/pages/ProfileEditor.js
+++ client/src/pages/ProfileEditor.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-// import { Redirect } from "react-router-dom";
+import { Redirect } from "react-router-dom";
 import API from "../utils/API";
 import Container from "../components/Container";
 import Col from "../components/Col";
@@ -8,7 +8,7 @@ import { Input, FormBtn } from "../components/Form";
 import '../styles/index.css';
 
 function ProfileEditor() {
-    // const [redirect, setRedirect] = useState(false);
+    const [redirect, setRedirect] = useState(false);
     const [formObject, setFormObject] = useState({});
 
     function handleInputChange(event) {
@@ -26,7 +26,7 @@ function ProfileEditor() {
                 profession: formObject.profession,
                 description: formObject.description
             })
-                .then(res => console.log("Success!"))
+                .then(res => setRedirect(true))
                 .catch(err => console.log(err));
         }
     };
@@ -38,19 +38,19 @@ function ProfileEditor() {
     };
 
     return (
-        // (redirect) ? <Redirect to="/"></Redirect> :
+        (redirect) ? <Redirect to="/dashboard"></Redirect> :
 
-        <div>
-            <div className="mt-4">
-            </div>
-            <p>Hello ADD USERNAME FROM DATABASE please complete your profile</p>
-            <form id="create-user-form">
-                <Container className="mt-3 px-5">
-                    <Row className="form-group">
-                        <Col size="4">
-                            <img class="card-img" alt="user thumbnail" src="_blank"></img>
-                        </Col>
-                        {/* <Col size="4">
+            <div>
+                <div className="mt-4">
+                </div>
+                <p>Hello ADD USERNAME FROM DATABASE please complete your profile</p>
+                <form id="create-user-form">
+                    <Container className="mt-3 px-5">
+                        <Row className="form-group">
+                            <Col size="4">
+                                <img class="card-img" alt="user thumbnail" src="_blank"></img>
+                            </Col>
+                            {/* <Col size="4">
                             <input
                                 className="form-control"
                                 type="text"
@@ -59,46 +59,46 @@ function ProfileEditor() {
                                 onChange={e => setUsername(e.target.value)}
                             />
                         </Col> */}
-                        <Col size="4">
-                            <Input
-                                onChange={handleInputChange}
-                                name="profession"
-                                placeholder="Profession"
-                            />
+                            <Col size="4">
+                                <Input
+                                    onChange={handleInputChange}
+                                    name="profession"
+                                    placeholder="Profession"
+                                />
+                            </Col>
+                        </Row>
+                        <Row className="form-group">
+                            <Col size="12">
+                                <Input
+                                    onChange={handleInputChange}
+                                    name="description"
+                                    placeholder="Description"
+                                />
+                            </Col>
+                        </Row>
+                    </Container>
+                    <Container className="mt-4">
+                        <Col size="6">
+                            <p>Profile Template Previews</p>
                         </Col>
-                    </Row>
-                    <Row className="form-group">
-                        <Col size="12">
-                            <Input
-                                onChange={handleInputChange}
-                                name="description"
-                                placeholder="Description"
-                            />
+                        <Col size="6">
+                            <p>Profile Template Descriptions</p>
                         </Col>
-                    </Row>
-                </Container>
-                <Container className="mt-4">
-                    <Col size="6">
-                        <p>Profile Template Previews</p>
-                    </Col>
-                    <Col size="6">
-                        <p>Profile Template Descriptions</p>
-                    </Col>
-                </Container>
-                <Container className="mt-4">
-                    <FormBtn
-                        disabled={!(formObject.profession && formObject.description)}
-                        onClick={handleFormSubmit}
-                    >
-                        Save Changes
+                    </Container>
+                    <Container className="mt-4">
+                        <FormBtn
+                            disabled={!(formObject.profession && formObject.description)}
+                            onClick={handleFormSubmit}
+                        >
+                            Save Changes
                     </FormBtn>
-                    <FormBtn
-                        onClick={discardForm}                    >
-                        Discard Changes
+                        <FormBtn
+                            onClick={discardForm}                    >
+                            Discard Changes
                     </FormBtn>
-                </Container>
-            </form>
-        </div>
+                    </Container>
+                </form>
+            </div>
     );
 };
 
