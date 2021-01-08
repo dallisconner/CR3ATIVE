b1f5a0527503123e87dff9d963b2bdf83b84758c
Allison Haver <allisonbhaver@gmail.com>
2021-01-08 00:32:22 -0500

    added styling to about page

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -5,11 +5,10 @@ import { Link } from "react-router-dom";
 function Footer() {
   return (
     <footer className="footer">
-      <p>Copyright
-          <Link className="navbar-brand" to="/about" style={{ color: '#FFF' }} >
-            Cr3ative
-        </Link>
-        2021</p>
+      <h5>
+        <Link className="navbar-brand" to="/about" style={{ color: '#FFF' }} >
+          Cr3ative</Link>
+        Copyright 2021</h5>
     </footer>
   );
 }
--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,6 +1,8 @@
 import { Link } from "react-router-dom";
 import Container from "../components/Container";
-import '../styles/index.css';
+import Col from "../components/Col";
+import Row from "../components/Row";
+import '../styles/index.css'
 import tristan from '../styles/tristan.jpeg';
 import Dallis from '../styles/Dallis.png';
 import jesse from '../styles/jesse.jpeg';
@@ -14,18 +16,30 @@ function About() {
                 Meet The CR3ATIVE Team:
             </p>
             <Container>
-                <div>
-                <img src={tristan} />
-                </div>
-                <div>
-                <img src={Dallis} />
-                </div>
-                <div>
-                <img src={jesse} />
-                </div>
-                <div>
-                <img src={allison} />
-                </div>
+                <Row>
+                    <Col size="6">
+                    <a target="_blank" href="https://github.com/teastman201">
+                        <img src={tristan} className="tristan" alt="tristan" /> </a>
+                        <p> Tristan Eastman</p>
+                    </Col>
+                    <Col size="6">
+                        <a target="_blank" href="https://github.com/dallisconner">
+                            <img src={Dallis} className="Dallis" alt="Dallis" /> </a>
+                        <p>Dallis Conner</p>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col size="6">
+                    <a target="_blank" href="https://github.com/jesseziros">
+                        <img src={jesse} className="jesse" alt="jesse" /></a>
+                        <p>Jesse Ziros</p>
+                    </Col>
+                    <Col size="6">
+                    <a target="_blank" href="https://github.com/haverab">
+                        <img src={allison} className="allison" alt="allison" /></a>
+                        <p>Allison Haver</p>
+                    </Col>
+                </Row>
             </Container>
 
             <Container className="mt-4">
